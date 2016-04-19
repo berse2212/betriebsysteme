b35f1651ff366daeb93573292ac7741bb1605668
Tobias <tobias@localhost>
2016-04-19 15:22:23 +0200

    s

---
+++ src/osmprun.c
@@ -0,0 +1,18 @@
+/*
+ * osmprun.c
+ *
+ *  Created on: 19.04.2016
+ *      Author: tobias
+ */
+
+
+void create(char*);
+
+void* allocate(int);
+
+void delete(void*);
+
+int main(int argc, char **argv) {
+
+}
+
